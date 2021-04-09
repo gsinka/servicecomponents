3f57187fa47f6a04a9102288569ef12b567d34d8
Bence, Mihucza <Bence.Mihucza-EXT@continental.com>
2021-04-09 21:45:39 +0200

    WebApplicationFactory first steps

--- src/ReferenceApplication2.AspNet/Program.cs
+++ src/ReferenceApplication2.AspNet/Program.cs
@@ -18,79 +18,84 @@ namespace ReferenceApplication2.AspNet
     {
         public static void Main(string[] args)
         {
-            new ServiceComponentsHostBuilder()
-                .UseDefault(
-                
-                    new [] { typeof(TestCommand).Assembly },
-                    new []{ typeof(TestCommandHandler).Assembly})
+            CreateHostBuilder(args).Build().Run();
+        }
 
-                // Use serilog for logging
-                .UseSerilog((context, log) => log
-                    .WriteTo.Console(LogEventLevel.Information)
-                    .WriteTo.Seq("http://localhost:5341", LogEventLevel.Verbose)
-                    .Enrich.FromLogContext()
-                    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-                    .MinimumLevel.Override("NHibernate", LogEventLevel.Warning)
-                    .MinimumLevel.Verbose())
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return new ServiceComponentsHostBuilder()
+                            .UseDefault(
 
-                // Add OpenApi
-                .AddOpenApi(
-                    (configuration, options) => options.SwaggerDoc("v1", new OpenApiInfo { Title = "ReferenceApplication2.AspNet", Version = "v1" }), 
-                    (configuration, options) => options.SwaggerEndpoint("/swagger/v1/swagger.json", "ReferenceApplication2.AspNet v1"))
+                                new[] { typeof(TestCommand).Assembly },
+                                new[] { typeof(TestCommandHandler).Assembly })
 
-                // Health check
-                .AddHealthCheck((configuration, check) => {
-                    check.AddRabbitMQ(rabbitConnectionString: "amqp://localhost:5672");
-                    check.AddRedis("localhost");
-                })
-                
-                // Add http sender
-                .AddHttpSender(new Uri("http://localhost:5000/api/generic"), "http")
+                            // Use serilog for logging
+                            .UseSerilog((context, log) => log
+                                .WriteTo.Console(LogEventLevel.Information)
+                                .WriteTo.Seq("http://localhost:5341", LogEventLevel.Verbose)
+                                .Enrich.FromLogContext()
+                                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                                .MinimumLevel.Override("NHibernate", LogEventLevel.Warning)
+                                .MinimumLevel.Verbose())
 
-                // Routing
-                .AddCommandRouter(command => "loopback")
-                .AddQueryRouter(query => "loopback")
-                .AddEventRouter(evnt => "loopback")
+                            // Add OpenApi
+                            .AddOpenApi(
+                                (configuration, options) => options.SwaggerDoc("v1", new OpenApiInfo { Title = "ReferenceApplication2.AspNet", Version = "v1" }),
+                                (configuration, options) => options.SwaggerEndpoint("/swagger/v1/swagger.json", "ReferenceApplication2.AspNet v1"))
 
-                // Redis
-                //.AddRedis(configuration => "localhost:6379")
+                            // Health check
+                            .AddHealthCheck((configuration, check) => {
+                                check.AddRabbitMQ(rabbitConnectionString: "amqp://localhost:5672");
+                                check.AddRedis("localhost");
+                            })
 
-                // Rabbit
-                //.AddRabbit("amqp://guest:guest@localhost:5672", "test2", "test-queue", "test-exchange", retryIntervals: new[] { 1000, 3000, 5000 })
-                .AddRabbit("amqp://guest:guest@localhost:5672", "test2", "test-queue", "test-exchange")
+                            // Add http sender
+                            .AddHttpSender(new Uri("http://localhost:5000/api/generic"), "http")
 
-                .RegisterCallback(builder => { builder.AddNewtonsoftJson(options => options.UseCamelCasing(true)); })
-                
-                .AddRedisDistributedCache("localhost")
-                
-                .RegisterCallback((context, builder) => {
+                            // Routing
+                            .AddCommandRouter(command => "loopback")
+                            .AddQueryRouter(query => "loopback")
+                            .AddEventRouter(evnt => "loopback")
 
-                    builder.AddRequestConstraints(request => request switch {
+                            // Redis
+                            //.AddRedis(configuration => "localhost:6379")
 
-                        LongCommand longCommand => new []{ "test1", "test2" },
-                        TestCommand testCommand => new[] { "test2" },
-                        _ => default
+                            // Rabbit
+                            //.AddRabbit("amqp://guest:guest@localhost:5672", "test2", "test-queue", "test-exchange", retryIntervals: new[] { 1000, 3000, 5000 })
+                            .AddRabbit("amqp://guest:guest@localhost:5672", "test2", "test-queue", "test-exchange")
 
-                    },(key, count) => (key, count) switch {
+                            .RegisterCallback(builder => { builder.AddNewtonsoftJson(options => options.UseCamelCasing(true)); })
 
-                        ("test1", _) when count > 0 => false,
-                        ("test2", _) when count > 0 => false,
-                        (_, _) => true
+                            .AddRedisDistributedCache("localhost")
 
-                    }, (key) => TimeSpan.FromSeconds(30));
-                })
+                            .RegisterCallback((context, builder) => {
 
-                // NHibernate
-                .AddNHibernate(
-                    configuration => "Server=localhost; Port=5432; Database=ref-app; User Id=postgres; Password=postgres", 
-                    map => map.FluentMappings.AddFromAssemblyOf<TestEntity>(),
-                    configuration => new SchemaUpdate(configuration).Execute(true, true))
-         
-                .AddPrometheusMetrics()
-                
-                .AddBadge()
+                                builder.AddRequestConstraints(request => request switch {
 
-                .Build(args).Run();
+                                    LongCommand longCommand => new[] { "test1", "test2" },
+                                    TestCommand testCommand => new[] { "test2" },
+                                    _ => default
+
+                                }, (key, count) => (key, count) switch {
+
+                                    ("test1", _) when count > 0 => false,
+                                    ("test2", _) when count > 0 => false,
+                                    (_, _) => true
+
+                                }, (key) => TimeSpan.FromSeconds(30));
+                            })
+
+                            // NHibernate
+                            .AddNHibernate(
+                                configuration => "Server=localhost; Port=5432; Database=ref-app; User Id=postgres; Password=postgres",
+                                map => map.FluentMappings.AddFromAssemblyOf<TestEntity>(),
+                                configuration => new SchemaUpdate(configuration).Execute(true, true))
+
+                            .AddPrometheusMetrics()
+
+                            .AddBadge()
+
+                            .CreateHostBuilder(args);
         }
     }
 }
--- src/ServiceComponents.AspNet/Wireup/ServiceComponentsAspHostBuilderExtensions.cs
+++ src/ServiceComponents.AspNet/Wireup/ServiceComponentsAspHostBuilderExtensions.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 using Serilog;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using Swashbuckle.AspNetCore.SwaggerUI;
