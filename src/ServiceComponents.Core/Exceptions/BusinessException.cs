336582b1294cf25b3580fdaad5bc3e9ed481d16b
Gabor Sinka <gabor.sinka@gmail.com>
2021-05-27 15:04:08 +0200

    business exception change

--- src/ServiceComponents.Core/Exceptions/BusinessException.cs
+++ src/ServiceComponents.Core/Exceptions/BusinessException.cs
@@ -5,6 +5,8 @@ namespace ServiceComponents.Core.Exceptions
 {
     public class BusinessException : Exception
     {
+        public int ErrorCode { get; }
+
         public BusinessException()
         { }
 
@@ -16,5 +18,15 @@ namespace ServiceComponents.Core.Exceptions
 
         public BusinessException(string message, Exception innerException) : base(message, innerException)
         { }
+
+        public BusinessException(int errorCode, string message) : base(message)
+        {
+            ErrorCode = errorCode;
+        }
+
+        public BusinessException(int errorCode, string message, Exception innerException) : base(message, innerException)
+        {
+            ErrorCode = errorCode;
+        }
     }
 }
