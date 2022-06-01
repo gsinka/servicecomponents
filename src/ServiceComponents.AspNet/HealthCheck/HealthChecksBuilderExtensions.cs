a75dcaba18a746837968f5d92e2dd30cea5c6c1e
Mihucza Bence (uids6410) <Bence.Mihucza-EXT@continental-corporation.com>
2022-06-01 09:58:12 +0200

    Added rabbitmq consumer health check

---
+++ src/ServiceComponents.AspNet/HealthCheck/HealthChecksBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using ServiceComponents.Infrastructure.Rabbit.HealthCheck;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class HealthChecksBuilderExtensions
+    {
+        public static IHealthChecksBuilder AddConsumers(this IHealthChecksBuilder builder, string name, IEnumerable<string> tags)
+        {
+            return builder.AddCheck<RabbitMQConsumerChecker>(name, tags: tags);
+        }
+    }
+}
---
+++ src/ServiceComponents.AspNet/HealthCheck/RabbitMQConsumerChecker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace ServiceComponents.Infrastructure.Rabbit.HealthCheck
+{
+    public class RabbitMQConsumerChecker : IHealthCheck
+    {
+        private readonly IEnumerable<IRabbitConsumer> _consumers;
+
+        public RabbitMQConsumerChecker(IEnumerable<IRabbitConsumer> consumers)
+        {
+            _consumers = consumers;
+        }
+
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken)
+        {
+            var data = new Dictionary<string, object>();
+            var result = HealthCheckResult.Healthy("All consumers are healthy", data: data);
+            foreach (var consumer in _consumers) {
+                cancellationToken.ThrowIfCancellationRequested();
+                data.Add(consumer.ConsumerTag, consumer.IsLive ? "Healthy" : "Unhealthy");
+                if (!consumer.IsLive) result = HealthCheckResult.Unhealthy("One or more consumer is not healthy", data: data);
+            }
+            return Task.FromResult(result);
+        }
+    }
+}
